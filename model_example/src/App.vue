12b4012518ac17cb422020627541edfdae258bb7
zhaojunhai <zhaojh@MacBook-Pro.local>
2016-07-01 23:42:25 +0800

    modal example

---
+++ model_example/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <modal :show.sync="showModal">
+      <h3 slot="header">custom header</h3>
+    </modal>
+  </div>
+</template>
+
+<script>
+  import Modal from 'components/Modal_component.vue'
+
+  export default {
+    // el: function() {return {el:'#app'};},
+    data: function() {
+      return {
+        showModal:false
+      };
+    },
+    components: {
+      'modal': Modal
+    }
+  }
+</script>
